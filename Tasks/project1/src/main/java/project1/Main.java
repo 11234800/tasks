5c567bd7bddf73a38253f61055e61a076681622e
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-03-14 22:31:21 +0200

    Delete Tasks/project1 directory

--- Tasks/project1/src/main/java/project1/Main.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-import project1.service.impl.ApplicationServiceImpl;
-
-public class Main {
-    public static void main(String[] args) {
-        new ApplicationServiceImpl().startApplication();
-    }
-}
--- Tasks/project1/src/main/java/project1/service/ApplicationService.java
+++
@@ -1,6 +0,0 @@
-package project1.service;
-
-public interface ApplicationService {
-
-    void startApplication();
-}
