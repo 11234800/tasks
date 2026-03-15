518173493c2e59f774b3066ec167535259a09abb
Zeyad Baleigh <zeyadbaleigh@gmail.com>
2026-03-15 15:44:07 +0200

    update

--- src/main/java/project1/Main.java
+++ src/main/java/project1/Main.java
@@ -5,5 +5,6 @@ import project1.service.impl.ApplicationServiceImpl;
 public class Main {
     public static void main(String[] args) {
         new ApplicationServiceImpl().startApplication();
+        System.out.println("hhhhh");
     }
 }
